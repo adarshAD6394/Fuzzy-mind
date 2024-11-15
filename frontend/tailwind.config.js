71dcaf33a7cdf65ab81ed778a2c44ab8b019a061
Adarsh Gupta <99598374+adarshAD6394@users.noreply.github.com>
2024-11-15 12:30:39 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        golden: '#FFD700', // Hex code for golden yellow
+      },
+    },
+    },
+  darkMode: 'class',
+  plugins: [],
+}
